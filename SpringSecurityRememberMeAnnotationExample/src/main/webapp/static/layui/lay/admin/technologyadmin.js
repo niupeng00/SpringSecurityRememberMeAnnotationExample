97af1536b52cf93041f730494635793d72811e86
niupeng00 <365527950@QQ.com>
2019-03-19 16:51:45 +0800

    spring5+springMvc5+hibername5+thymeleaf5+springSecurity5+layui2.4.5+mysql8.0.15无XML集成

---
+++ SpringSecurityRememberMeAnnotationExample/src/main/webapp/static/layui/lay/admin/technologyadmin.js
@@ -0,0 +1,88 @@
+/** layuiAdmin.std-v1.2.1 LPPL License By http://www.layui.com/admin/ */
+;layui.define(["table", "form"], function (e) {
+    var t = layui.$, i = layui.table;
+    layui.form;
+    i.render({
+        elem: "#LAY-user-back-manage",
+        url: "technologyJson",
+        toolbar: '#test-table-toolbar-toolbarDemo',
+        cols: [
+            [
+                //{type: "checkbox", fixed: "left"},
+                //{field: "id", width: 80, title: "ID", sort: !0},
+                {field: "serverName", title: "服務器名稱"},
+                {field: "link", title: "連接方式", templet: "#titleLink"},
+                {field: "ip", title: "IP地質"},
+                {field: "port", title: "端口號"},
+                {field: "username", title: "用戶名"},
+                {field: "password", title: "密碼"},
+                {title: "操作", width: 150, align: "center", fixed: "right", toolbar: "#table-useradmin-admin"}
+            ]
+        ],
+        text: "对不起，加载出现异常！"
+    }), i.on("tool(LAY-user-back-manage)", function (e) {
+        e.data;
+        if ("del" === e.event) layer.prompt({formType: 1, title: "敏感操作，请验证口令"}, function (t, i) {
+            layer.close(i), layer.confirm("确定删除此管理员？", function (t) {
+                console.log(e), e.del(), layer.close(t)
+            })
+        }); else if ("edit" === e.event) {
+            t(e.tr);
+            layer.open({
+                type: 2,
+                title: "编辑管理员",
+                content: "../../../views/user/administrators/adminform.html",
+                area: ["420px", "420px"],
+                btn: ["确定", "取消"],
+                yes: function (e, t) {
+                    var l = window["layui-layer-iframe" + e], r = "LAY-user-back-submit",
+                        n = t.find("iframe").contents().find("#" + r);
+                    l.layui.form.on("submit(" + r + ")", function (t) {
+                        t.field;
+                        i.reload("LAY-user-front-submit"), layer.close(e)
+                    }), n.trigger("click")
+                },
+                success: function (e, t) {
+                }
+            })
+        }
+    }), i.render({
+        elem: "#LAY-user-back-role",
+        url: layui.setter.base + "json/useradmin/role.js",
+        cols: [[{type: "checkbox", fixed: "left"}, {field: "id", width: 80, title: "ID", sort: !0}, {
+            field: "rolename",
+            title: "角色名"
+        }, {field: "limits", title: "拥有权限"}, {field: "descr", title: "具体描述"}, {
+            title: "操作",
+            width: 150,
+            align: "center",
+            fixed: "right",
+            toolbar: "#table-useradmin-admin"
+        }]],
+        text: "对不起，加载出现异常！"
+    }), i.on("tool(LAY-user-back-role)", function (e) {
+        e.data;
+        if ("del" === e.event) layer.confirm("确定删除此角色？", function (t) {
+            e.del(), layer.close(t)
+        }); else if ("edit" === e.event) {
+            t(e.tr);
+            layer.open({
+                type: 2,
+                title: "编辑角色",
+                content: "../../../views/user/administrators/roleform.html",
+                area: ["500px", "480px"],
+                btn: ["确定", "取消"],
+                yes: function (e, t) {
+                    var l = window["layui-layer-iframe" + e],
+                        r = t.find("iframe").contents().find("#LAY-user-role-submit");
+                    l.layui.form.on("submit(LAY-user-role-submit)", function (t) {
+                        t.field;
+                        i.reload("LAY-user-back-role"), layer.close(e)
+                    }), r.trigger("click")
+                },
+                success: function (e, t) {
+                }
+            })
+        }
+    }), e("technologyadmin", {})
+});
